fb113c79843c40afdf1d0d122d0ff9182566ea5e
Éric Massé <Ericmas001@gmail.com>
2018-04-03 08:16:01 -0400

    Web.config override Filters

---
+++ WebApiUtil/Controllers/ExceptionsController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Security.Authentication;
+using System.Web.Http;
+
+namespace WebApiUtil.Controllers
+{
+    public class ExceptionsController : ApiController
+    {
+        // GET api/exceptions/{type}
+        public string Get(string type)
+        {
+            switch (type)
+            {
+                case "IMPL":
+                    throw new NotImplementedException();
+                case "AUTH":
+                    throw new AuthenticationException();
+            }
+            throw new Exception();
+        }
+    }
+}
